1054af6b4126b8eb9a27238c4fa5105b74203ca4
heum23 <jinsoon6230@naver.com>
2025-03-30 19:46:14 +0900

    social login

--- src/pages/auth/google/callback/index.tsx
+++ src/pages/auth/google/callback/index.tsx
@@ -1,106 +1,35 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
-interface UserInfo {
-  name: string;
-  email: string;
-  picture: string;
-}
-
+import axios from "axios";
+import Join from "@/features/JoinPage/Join";
 const GoogleCallback = () => {
   const router = useRouter();
-  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     const { code } = router.query;
 
     if (typeof code === "string") {
-      fetchAccessToken(code);
+      console.log(code);
+      login(code);
     } else {
-      setError("잘못된 요청입니다.");
+      console.log("잘못된 요청입니다.");
     }
   }, [router.query]);
-
-  const fetchAccessToken = async (code: string) => {
-    const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-    const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
-    const REDIRECT_URI = encodeURIComponent(
-      "http://localhost:3000/auth/google/callback"
-    );
-
-    const tokenUrl = `https://oauth2.googleapis.com/token`;
-
-    const body = new URLSearchParams();
-    body.append("code", code);
-    // body.append("client_id", GOOGLE_CLIENT_ID);
-    // body.append("client_secret", GOOGLE_CLIENT_SECRET);
-    body.append("redirect_uri", REDIRECT_URI);
-    body.append("grant_type", "authorization_code");
-
-    body.append("redirect_uri", REDIRECT_URI);
-    body.append("grant_type", "authorization_code");
-
-    try {
-      const response = await fetch(tokenUrl, {
-        method: "POST",
-        body: body,
-      });
-
-      const data = await response.json();
-
-      if (data.access_token) {
-        fetchUserInfo(data.access_token);
-      } else {
-        setError("액세스 토큰을 받는 데 실패했습니다.");
-      }
-    } catch (err) {
-      setError("에러가 발생했습니다.");
-    }
-  };
-
-  const fetchUserInfo = async (accessToken: string) => {
-    try {
-      const userInfoResponse = await fetch(
-        "https://www.googleapis.com/oauth2/v3/userinfo",
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
+  const login = (code: string) => {
+    axios
+      .post("http://localhost:5000/auth/login/google", code) // 서버 URL
+      .then((res) => {
+        if (res.data) {
+          return <Join />;
+        } else {
+          console.log("로그인 성공", res.data);
+          router.push("/");
         }
-      );
-
-      const userInfoData = await userInfoResponse.json();
-
-      if (userInfoData.sub) {
-        setUserInfo({
-          name: userInfoData.name,
-          email: userInfoData.email,
-          picture: userInfoData.picture,
-        });
-      } else {
-        setError("사용자 정보를 가져오는 데 실패했습니다.");
-      }
-    } catch (err) {
-      setError("에러가 발생했습니다.");
-    }
+      })
+      .catch((error) => {
+        console.error("로그인 실패 :", error);
+      });
   };
-
-  return (
-    <div>
-      <h1>구글 로그인 결과</h1>
-      {error && <p>{error}</p>}
-      {userInfo ? (
-        <div>
-          <p>사용자 이름: {userInfo.name}</p>
-          <p>사용자 이메일: {userInfo.email}</p>
-          <img src={userInfo.picture} alt="profile" />
-        </div>
-      ) : (
-        <p>로그인 중...</p>
-      )}
-    </div>
-  );
 };
 
 export default GoogleCallback;
--- src/pages/auth/kakao/callback/index.tsx
+++ src/pages/auth/kakao/callback/index.tsx
@@ -1,100 +1,35 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
-interface UserInfo {
-  nickname: string;
-  email: string;
-  profile_image: string;
-}
-
+import axios from "axios";
+import Join from "@/features/JoinPage/Join";
 const KakaoCallback = () => {
   const router = useRouter();
-  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-  const [error, setError] = useState<string>("");
 
   useEffect(() => {
-    const { code, state } = router.query;
-
-    console.log(code, state); // 추가된 로그를 통해 확인
+    const { code } = router.query;
 
     if (typeof code === "string") {
-      fetchAccessToken(code);
+      console.log(code);
+      login(code);
     } else {
-      setError("잘못된 요청입니다.");
+      console.log("잘못된 요청입니다.");
     }
   }, [router.query]);
-
-  const fetchAccessToken = async (code: string) => {
-    const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-    const KAKAO_CLIENT_SECRET = process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET;
-    const REDIRECT_URI = encodeURIComponent(
-      "http://localhost:3000/auth/kakao/callback"
-    );
-
-    const tokenUrl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${code}`;
-
-    try {
-      const response = await fetch(tokenUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      });
-
-      const data = await response.json();
-
-      if (data.access_token) {
-        fetchUserInfo(data.access_token);
-      } else {
-        setError("액세스 토큰을 받는 데 실패했습니다.");
-      }
-    } catch (err) {
-      setError("에러가 발생했습니다.");
-    }
-  };
-
-  const fetchUserInfo = async (accessToken: string) => {
-    try {
-      const userInfoResponse = await fetch(
-        "https://kapi.kakao.com/v2/user/me",
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
+  const login = (code: string) => {
+    axios
+      .post("http://localhost:5000/auth/login/kakao", code) // 서버 URL
+      .then((res) => {
+        if (res.data) {
+          return <Join />;
+        } else {
+          console.log("로그인 성공", res.data);
+          router.push("/");
         }
-      );
-
-      const userInfoData = await userInfoResponse.json();
-
-      if (userInfoData.id) {
-        setUserInfo({
-          nickname: userInfoData.properties.nickname,
-          email: userInfoData.kakao_account.email,
-          profile_image: userInfoData.properties.profile_image,
-        });
-      } else {
-        setError("사용자 정보를 가져오는 데 실패했습니다.");
-      }
-    } catch (err) {
-      setError("에러가 발생했습니다.");
-    }
+      })
+      .catch((error) => {
+        console.error("로그인 실패 :", error);
+      });
   };
-
-  return (
-    <div>
-      <h1>카카오 로그인 결과</h1>
-      {error && <p>{error}</p>}
-      {userInfo ? (
-        <div>
-          <p>사용자 이름: {userInfo.nickname}</p>
-          <p>사용자 이메일: {userInfo.email}</p>
-          <img src={userInfo.profile_image} alt="profile" />
-        </div>
-      ) : (
-        <p>로그인 중...</p>
-      )}
-    </div>
-  );
 };
 
 export default KakaoCallback;
