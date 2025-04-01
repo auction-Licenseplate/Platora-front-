1633554b7f2f505e1395d69191441e15f203a7eb
heum23 <jinsoon6230@naver.com>
2025-04-01 22:39:51 +0900

    kakao

--- src/components/SocialLogin/index.tsx
+++ src/components/SocialLogin/index.tsx
@@ -4,48 +4,57 @@ import axios from "axios";
 import PlusInfo from "@/features/PlusInfo";
 
 interface SocialCallbackProps {
-  type: string; // code는 string 타입으로 지정
+  type: string;
 }
 
 const SocialCallback = ({ type }: SocialCallbackProps) => {
   const router = useRouter();
-  const [isSuccess, setIsSuccess] = useState(false); // 로그인 성공 여부 상태
+  const [isSuccess, setIsSuccess] = useState(false);
   const [user, setUser] = useState("");
 
   useEffect(() => {
+    if (!router.isReady) return; // router.query가 준비될 때까지 대기
+
     const { code } = router.query;
     if (typeof code === "string") {
-      console.log("Received code:", code);
-      login(code); // code 전달
+      login(code);
     } else {
       console.log("잘못된 요청입니다.");
     }
-  }, [router.query]);
+  }, [router.isReady, router.query]);
 
   const login = (code: string) => {
-    axios
-      .post(
-        `http://localhost:5000/auth/login/${type}`,
-        { code },
-        { withCredentials: true }
-      ) // 서버 URL
-      .then((res) => {
-        if ((res.data.message = "가입되지 않은 유저")) {
-          setIsSuccess(false);
-          setUser(res.data.user.id);
-          console.log(user);
-        } else {
-          setIsSuccess(true);
-          router.push("/");
-        }
-      })
-      .catch((error) => {
-        console.error("로그인 실패 :", error);
-      });
+    try {
+      axios
+        .post(
+          `http://localhost:5000/auth/login/${type}`,
+          { code },
+          { withCredentials: true }
+        )
+        .then((res) => {
+          console.log("응답 데이터:", res);
+
+          // 조건 수정
+          if (typeof res.data.user === "number") {
+            // user가 undefined, null, 또는 falsy 값인 경우
+            console.log("로그인 실패, user가 없습니다.");
+            setUser(res.data.user);
+            setIsSuccess(false); // 로그인 실패 상태
+          } else {
+            // user가 있을 경우 성공 처리
+            console.log("로그인 성공");
+            setUser(res.data.user);
+            setIsSuccess(true); // 로그인 성공 상태
+          }
+        });
+    } catch (error) {
+      console.error("로그인 실패:", error);
+      setIsSuccess(false); // 로그인 실패 상태
+    }
   };
 
   if (!isSuccess) {
-    return <PlusInfo userid={user} />; // 로그인 성공 시 Join 컴포넌트를 렌더링
+    return <PlusInfo userid={user} />; // 로그인 성공 시 PlusInfo 컴포넌트 렌더링
   }
 
   return <div>로딩 중...</div>; // 로딩 중 화면 표시
