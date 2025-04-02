ab8676700c45386fce97f1c46b57b6cc965cedec
heum23 <jinsoon6230@naver.com>
2025-04-02 19:12:03 +0900

    Update index.tsx

--- src/components/MyInfo/index.tsx
+++ src/components/MyInfo/index.tsx
@@ -10,7 +10,7 @@ import { UploadOutlined } from "@ant-design/icons";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
-
+import AiPoint from "@/features/MyPageManeger/AiPoint";
 import { loadTossPayments } from "@tosspayments/payment-sdk";
 
 // 이미지
