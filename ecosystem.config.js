eac4a28b4ff221ecdf8a0b22c863e0a6a635553c
heum23 <jinsoon6230@naver.com>
2025-04-17 16:52:42 +0900

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
       name: "next-app",
       script: "npm",
       args: "run start",
+      instances: 1,
+      exec_mode: "fork",
+      watch: true,
       env: {
         NODE_ENV: "production",
       },
