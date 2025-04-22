2d7e880ba96006b82871f55e14c3bbe368ca506c
heum23 <jinsoon6230@naver.com>
2025-04-22 22:45:00 +0900

    Update index.tsx

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -16,14 +16,14 @@ const MyPage = () => {
     if (!token && !isModalShown.current) {
       isModalShown.current = true;
 
-      Modal.warning({
-        centered: true,
-        title: "로그인 ",
-        content: "로그인 후 다시 시도해주세요.",
-        onOk: () => {
-          router.push("/login");
-        },
-      });
+      // Modal.warning({
+      //   centered: true,
+      //   title: "로그인 ",
+      //   content: "로그인 후 다시 시도해주세요.",
+      //   onOk: () => {
+      //     router.push("/login");
+      //   },
+      // });
     }
   }, [token]);
 
