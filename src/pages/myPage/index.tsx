184e2daa025cfaf6c1f8d80a3218a283bcb05828
heum23 <jinsoon6230@naver.com>
2025-04-23 19:39:11 +0900

    Update index.tsx

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -23,19 +23,19 @@ const MyPage = () => {
 
   console.log("mypage 토큰 : ", token);
 
-  useEffect(() => {
-    if (token === undefined) {
-      isModalShown.current = true;
-      Modal.warning({
-        centered: true,
-        title: "로그인 ",
-        content: "로그인 후 다시 시도해주세요.",
-        onOk: () => {
-          router.push("/login");
-        },
-      });
-    }
-  }, [token]);
+  // useEffect(() => {
+  //   if (token === undefined) {
+  //     isModalShown.current = true;
+  //     Modal.warning({
+  //       centered: true,
+  //       title: "로그인 ",
+  //       content: "로그인 후 다시 시도해주세요.",
+  //       onOk: () => {
+  //         router.push("/login");
+  //       },
+  //     });
+  //   }
+  // }, [token]);
 
   return <MyPageContainer />;
 };
