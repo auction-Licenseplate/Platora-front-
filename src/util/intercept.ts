5d3264c3b4612029264c3704363bffe3e07d3491
heum23 <jinsoon6230@naver.com>
2025-04-23 18:46:20 +0900

    Update intercept.ts

--- src/util/intercept.ts
+++ src/util/intercept.ts
@@ -7,17 +7,17 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     const status = error.response?.status;
-
-    if (status === 401) {
-      Modal.warning({
-        centered: true,
-        title: "로그인",
-        content: "로그인 후 다시 시도해주세요.",
-        onOk: () => {
-          window.location.href = "/login";
-        },
-      });
-    }
+    console.log(error);
+    // if (status === 401) {
+    //   Modal.warning({
+    //     centered: true,
+    //     title: "로그인",
+    //     content: "로그인 후 다시 시도해주세요.",
+    //     onOk: () => {
+    //       window.location.href = "/login";
+    //     },
+    //   });
+    // }
 
     return Promise.reject(error);
   }
