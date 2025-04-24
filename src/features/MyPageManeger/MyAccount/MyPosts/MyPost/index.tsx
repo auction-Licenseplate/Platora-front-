ac697d92e5706b0833cc29f549b7bb800507d822
jeong0300 <dltnwjd8898@naver.com>
2025-04-24 19:58:21 +0900

    feat: test

--- src/features/MyPageManeger/MyAccount/MyPosts/MyPost/index.tsx
+++ src/features/MyPageManeger/MyAccount/MyPosts/MyPost/index.tsx
@@ -119,7 +119,7 @@ const MyPost = ({
           {
             id: post.auctionID || post.auctionId,
             userId: post.userId,
-            status: !likedMap[key],
+            status: likedMap[key],
           },
           {
             withCredentials: true,
